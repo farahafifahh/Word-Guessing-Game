838d6d51b2c504ff7412b94cad3de5b5e26f2194
farahafifahh <farahafifah@student.usm.my>
2023-01-09 01:18:56 +0800

    Initial Commit

---
+++ Word Guessing Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Word Guessing Game
+//
+//  Created by Farah Afifah on 09/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
